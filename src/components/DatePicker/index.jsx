f4d54892be78a8e57570ac27f6c2c9d7f71c8183
chris <5sensprod@gmail.com>
2023-08-11 15:37:15 +0200

    import du composant perso dans DateInputField

--- src/components/DatePicker/index.jsx
+++ src/components/DatePicker/index.jsx
@@ -2,24 +2,31 @@ import React, { useState } from 'react'
 import styles from './DatePicker.module.css'
 import Calendar from './Calendar'
 
-function DatePicker() {
+function DatePicker({ value, onChange }) {
   const [showCalendar, setShowCalendar] = useState(false)
-  const [selectedDate, setSelectedDate] = useState(null)
 
   const toggleCalendar = () => {
     setShowCalendar(!showCalendar)
   }
 
+  const handleDateChange = (date) => {
+    onChange(date)
+    setShowCalendar(false)
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.containerInput}>
-        <input type="text" value={selectedDate || ''} readOnly />
+        <input type="text" value={value || ''} readOnly />
         <button onClick={toggleCalendar} className={styles.calendarButton}>
           📅
         </button>
       </div>
       {showCalendar && (
-        <Calendar selectDate={setSelectedDate} closeCalendar={toggleCalendar} />
+        <Calendar
+          selectDate={handleDateChange}
+          closeCalendar={toggleCalendar}
+        />
       )}
     </div>
   )
--- src/components/EmployeeForm/DateInputField.jsx
+++ src/components/EmployeeForm/DateInputField.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import styles from './EmployeeForm.module.css'
-import DatePicker from 'react-datepicker'
-import 'react-datepicker/dist/react-datepicker.css'
+// import DatePicker from 'react-datepicker'
+// import 'react-datepicker/dist/react-datepicker.css'
+import DatePicker from '../DatePicker'
 
 const DateInputField = ({ name, value, label, onChange, error }) => (
   <div className={styles.label}>
     <p>{label}</p>
     <DatePicker
-      selected={value ? new Date(value) : null}
+      value={value ? new Date(value).toISOString().split('T')[0] : ''}
       onChange={(date) =>
         onChange({
           target: {
@@ -16,7 +17,6 @@ const DateInputField = ({ name, value, label, onChange, error }) => (
           },
         })
       }
-      showYearDropdown
     />
     {error && <span className={styles.errorMessage}>{error}</span>}
   </div>
