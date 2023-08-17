a6b71c964b49196b35ad857a90f7b1d11264a852
chris <5sensprod@gmail.com>
2023-08-17 03:21:03 +0200

    finalisation plgin

--- src/components/DatePicker/views/DaysView.jsx
+++ src/components/DatePicker/views/DaysView.jsx
@@ -1,11 +1,7 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import styles from '../Calendar.module.css'
-import { handleNavigationKeys } from '../utils/handleNavigationKeys'
-import { updateMonth, handleTabKey } from '../utils/DaysViewUtils'
-import {
-  useInitialFocusEffect,
-  useNavigationDirectionEffect,
-} from '../hooks/daysViewHooks'
+import { handleNavigationKeys } from '../utils'
+import { getDaysInMonth } from '../utils/dateFunctions'
 
 function DaysView({
   totalSlots,
@@ -15,16 +11,35 @@ function DaysView({
   currentMonth,
 }) {
   const daysRefs = useRef([])
-  const [navigationDirection, setNavigationDirection] = useState(null)
+  const [navigationDirection, setNavigationDirection] = useState(null) // 'forward' ou 'backward'
 
-  useInitialFocusEffect(currentMonth, totalSlots, daysRefs)
-  useNavigationDirectionEffect(
-    navigationDirection,
-    currentMonth,
-    daysRefs,
-    setNavigationDirection,
-  )
+  useEffect(() => {
+    // Si le calendrier est visible (ou vient d'être ouvert)
+    const firstNonGrayedDayIndex = totalSlots.findIndex((day) => !day.isGrayed)
+    if (firstNonGrayedDayIndex !== -1) {
+      setTimeout(() => {
+        daysRefs.current[firstNonGrayedDayIndex]?.focus()
+      }, 0)
+    }
+  }, [currentMonth, totalSlots])
 
+  useEffect(() => {
+    if (navigationDirection === 'forward') {
+      setTimeout(() => {
+        daysRefs.current[0]?.focus()
+        setNavigationDirection(null) // Réinitialisez la direction de navigation
+      }, 0)
+    } else if (navigationDirection === 'backward') {
+      const daysInPrevMonth = getDaysInMonth(
+        currentMonth.getFullYear(),
+        currentMonth.getMonth(),
+      )
+      setTimeout(() => {
+        daysRefs.current[daysInPrevMonth - 1]?.focus()
+        setNavigationDirection(null) // Réinitialisez la direction de navigation
+      }, 0)
+    }
+  }, [navigationDirection, currentMonth])
   const handleDayKeyDown = (e, index) => {
     handleNavigationKeys(
       e,
@@ -34,57 +49,57 @@ function DaysView({
       daysRefs.current,
     )
 
-    if (e.key === 'Tab') {
-      const { direction } = handleTabKey(
-        e,
-        totalSlots[index],
-        index,
-        currentMonth,
-      )
-      if (direction) {
-        const newMonth = updateMonth(currentMonth, direction)
-        setCurrentMonth(newMonth)
-        setNavigationDirection(direction)
-      }
+    const day = totalSlots[index]
+    const daysInCurrentMonth = getDaysInMonth(
+      currentMonth.getFullYear(),
+      currentMonth.getMonth(),
+    )
+
+    const isLastDayOfCurrentMonth =
+      day.number === daysInCurrentMonth && !day.isGrayed
+    const isFirstDayOfCurrentMonth = day.number === 1 && !day.isGrayed
+
+    if (e.key === 'Tab' && !e.shiftKey && isLastDayOfCurrentMonth) {
+      e.preventDefault()
+      setCurrentMonth((prevMonth) => {
+        const newMonth = new Date(prevMonth)
+        newMonth.setMonth(prevMonth.getMonth() + 1)
+        return newMonth
+      })
+      setNavigationDirection('forward')
+    } else if (e.key === 'Tab' && e.shiftKey && isFirstDayOfCurrentMonth) {
+      e.preventDefault()
+      setCurrentMonth((prevMonth) => {
+        const newMonth = new Date(prevMonth)
+        newMonth.setMonth(prevMonth.getMonth() - 1)
+        return newMonth
+      })
+      setNavigationDirection('backward')
     }
   }
 
   return (
     <div className={styles.daysContainer}>
       {reorderedDays.map((day) => (
-        <DayHeader key={day} day={day} />
+        <div className={styles.header} key={day}>
+          {day}
+        </div>
       ))}
       {totalSlots.map((day, index) => (
-        <Day
+        <div
           key={index}
-          day={day}
-          onDayClick={(event) => {
+          className={day.isGrayed ? styles.grayedDay : styles.day}
+          onClick={(event) => {
             event.stopPropagation()
             if (!day.isGrayed) chooseDate(day.number)
           }}
-          onDayKeyDown={(e) => handleDayKeyDown(e, index)}
-          refElement={(el) => (daysRefs.current[index] = el)}
-        />
+          onKeyDown={(e) => handleDayKeyDown(e, index)}
+          ref={(el) => (daysRefs.current[index] = el)}
+          tabIndex={day.isGrayed ? -1 : 0}
+        >
+          {day.number}
+        </div>
       ))}
-    </div>
-  )
-}
-
-function DayHeader({ day }) {
-  return <div className={styles.header}>{day}</div>
-}
-
-function Day({ day, onDayClick, onDayKeyDown, refElement }) {
-  const className = day.isGrayed ? styles.grayedDay : styles.day
-  return (
-    <div
-      className={className}
-      onClick={onDayClick}
-      onKeyDown={onDayKeyDown}
-      ref={refElement}
-      tabIndex={day.isGrayed ? -1 : 0}
-    >
-      {day.number}
     </div>
   )
 }
