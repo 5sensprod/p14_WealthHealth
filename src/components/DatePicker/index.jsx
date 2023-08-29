cd164da010879342f60d6f75833d919335f62797
chris <5sensprod@gmail.com>
2023-08-29 13:33:04 +0200

    refacto suite

--- src/components/DatePicker/index.jsx
+++ src/components/DatePicker/index.jsx
@@ -67,24 +67,17 @@ function DatePicker({
   }, [value, setInput])
 
   // 3. Handlers
-  const handleDateSelect = (date) => {
-    const actualDate = typeof date === 'string' ? new Date(date) : date
-
-    setInput(actualDate) // Ceci affecte la valeur à l'input visuel, donc utilisez `dateFormat`
-    setSelectedDate(actualDate)
-    closeCalendar()
-
-    // Validez la nouvelle valeur ici avec le format d'affichage (dateFormat)
-    validate(formatDatePickerDate(actualDate, dateFormat))
-
-    // Quand vous envoyez la valeur en dehors, utilisez `outputFormat`
-    onChange({
-      target: {
-        name,
-        value: formatDatePickerDate(actualDate, outputFormat),
-      },
-    })
-  }
+  const onDateSelect = handleDateSelect(
+    setInput,
+    setSelectedDate,
+    closeCalendar,
+    validate,
+    formatDatePickerDate,
+    dateFormat,
+    onChange,
+    name,
+    outputFormat,
+  )
 
   const handleInputChange = (e) => {
     const newValue = e.target.value
@@ -203,7 +196,7 @@ function DatePicker({
       {showCalendar && (
         <Calendar
           startOfWeek={startOfWeek}
-          selectDate={handleDateSelect}
+          selectDate={onDateSelect}
           closeCalendar={closeCalendar}
           useIcons={useIcons}
           translations={translations}
