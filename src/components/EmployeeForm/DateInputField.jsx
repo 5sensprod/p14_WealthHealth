954f6dc8b585f9d0083192a2c526dac7896ce9de
chris <5sensprod@gmail.com>
2023-09-08 14:27:16 +0200

    modif css

--- src/components/EmployeeForm/DateInputField.jsx
+++ src/components/EmployeeForm/DateInputField.jsx
@@ -15,7 +15,7 @@ const DateInputField = ({ name, value, label, onChange, className, error }) => {
         value={value}
         onChange={onChange}
         className={className}
-        designType={'glass'}
+        designType={'default'}
         yearBlockSize={16}
         useIcons={true}
         startOfWeek={0}
