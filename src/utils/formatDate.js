9c3a75be1a434c000a061f604577afcb5aabd898
chris <5sensprod@gmail.com>
2023-09-10 10:37:59 +0200

    ajout jsdoc utils

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,3 +1,13 @@
+/**
+ * Formate une date selon le format spécifié.
+ *
+ * @function
+ * @param {Date} date - La date à formater.
+ * @param {string} [format='YYYY-MM-DD'] - Le format de date souhaité. Par défaut, il est 'YYYY-MM-DD'.
+ * @returns {string|null} La date formatée ou null si l'argument "date" n'est pas une instance de Date.
+ * @example
+ * formatDate(new Date(2023, 5, 1), 'DD-MM-YYYY') // "01-06-2023"
+ */
 export const formatDate = (date, format = 'YYYY-MM-DD') => {
   if (!(date instanceof Date)) return null
 
@@ -14,6 +24,15 @@ export const formatDate = (date, format = 'YYYY-MM-DD') => {
   }
 }
 
+/**
+ * Formate une date (sous forme de chaîne) avec des slashes comme séparateurs.
+ *
+ * @function
+ * @param {string} dateString - La date sous forme de chaîne à formater.
+ * @returns {string} La date formatée avec des slashes.
+ * @example
+ * formatDateWithSlashes('2023-06-01') // "2023/06/01"
+ */
 export const formatDateWithSlashes = (dateString) => {
   const date = new Date(dateString)
   return `${date.getFullYear()}/${String(date.getMonth() + 1).padStart(
--- src/utils/formatFieldNames.js
+++ src/utils/formatFieldNames.js
@@ -1,3 +1,20 @@
+/**
+ * Objet qui associe les noms des champs à leur nom affiché formaté.
+ * Ceci est utile pour présenter les champs de manière conviviale
+ * (par exemple, dans des tableaux, des formulaires ou d'autres éléments de l'interface utilisateur).
+ *
+ * @typedef {Object} NomsDeChampsFormates
+ * @property {string} firstName - Nom affiché pour le champ du prénom.
+ * @property {string} lastName - Nom affiché pour le champ du nom de famille.
+ * @property {string} dateOfBirth - Nom affiché pour le champ de la date de naissance.
+ * @property {string} startDate - Nom affiché pour le champ de la date de début.
+ * @property {string} street - Nom affiché pour le champ de la rue.
+ * @property {string} city - Nom affiché pour le champ de la ville.
+ * @property {string} state - Nom affiché pour le champ de l'état ou de la région.
+ * @property {string} zipCode - Nom affiché pour le champ du code postal.
+ * @property {string} department - Nom affiché pour le champ du département.
+ */
+
 export const formattedFieldNames = {
   firstName: 'First Name',
   lastName: 'Last Name',
