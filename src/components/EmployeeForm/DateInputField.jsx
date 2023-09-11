1271c948241fb3da51fea52f94565bebcd1e03ae
chris <5sensprod@gmail.com>
2023-09-11 17:27:54 +0200

    Ajout du sous-module react-custom-datepicker

--- src/components/EmployeeForm/DateInputField.jsx
+++ src/components/EmployeeForm/DateInputField.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styles from './EmployeeForm.module.css'
-import DatePicker from '@5sensprod/react-custom-datepicker'
-// import DatePicker from '../DatePicker'
+// import DatePicker from '@5sensprod/react-custom-datepicker'
+import DatePicker from '../DatePicker/src'
 
 /**
  * Composant représentant un champ d'entrée de date.
